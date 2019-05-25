14c62bc0b833603bc3264650c3e28be854a8e97c
Anthony Anicotte <anthony.anicotte@epitech.eu>
2019-05-25 16:28:53 +0200

    added the || && ; operand for the command prompt

--- src/parser_and_or_semicolon.c
+++ src/parser_and_or_semicolon.c
@@ -1,8 +1,8 @@
 /*
 ** EPITECH PROJECT, 2019
-** 
+** 42sh
 ** File description:
-** 
+** parser_and_or_semicolumn
 */
 
 #include <stdlib.h>
@@ -13,8 +13,7 @@ int count_str_left(char **tab, int act_cpt)
     int nbr = 0;
 
     while (tab[act_cpt] && strcmp(tab[act_cpt], "&&") != 0 &&
-        strcmp(tab[act_cpt], "||") != 0 &&
-        strcmp(tab[act_cpt], ";") != 0) {
+        strcmp(tab[act_cpt], "||") != 0 && strcmp(tab[act_cpt], ";") != 0) {
         nbr++;
         act_cpt++;
     }
@@ -37,17 +36,43 @@ char **get_next_tab(int *i, char **cmd_parsed)
     return (tab);
 }
 
+void move_to_next_operand(int result, char **tab, int *i)
+{
+    printf("%s\nresult = %d\n", tab[(*i)], result);
+    if (tab[(*i)] && strcmp(tab[(*i)], "&&") == 0) {
+        if (result == VALID)
+            return;
+        (*i)++;
+        while (strcmp(tab[(*i)], "||") == 1 || strcmp(tab[(*i)], ";") == 1)
+            (*i)++;
+        return;
+    } else if (tab[(*i)] && strcmp(tab[(*i)], "||") == 0) {
+        (*i)++;
+        if (result == VALID) {
+            while (tab[(*i)] && strcmp(tab[(*i)], ";") == 1)
+                (*i)++;
+            return;
+        }
+        while (tab[(*i)] && strcmp(tab[(*i)], "||") == 1 &&
+            strcmp(tab[(*i)], ";") == 1)
+            (*i)++;
+        return;
+    }
+}
+
 void get_str(char **cmd_parsed, data_t *data)
 {
     bool detect = false;
     char **my_tab = NULL;
+    int result = -1;
 
     for (int i = 0; cmd_parsed[i]; i++) {
         my_tab = get_next_tab(&i, cmd_parsed);
         my_tab = replace_environnement_var(my_tab, data, &detect);
         if (detect == true)
             cmd_parsed[i + 1] = NULL;
-        get_array_from_and_or_final(data, my_tab);
+        result = get_array_from_and_or_final(data, my_tab);
+        move_to_next_operand(result, cmd_parsed, &i);
         if (!cmd_parsed[i])
             return;
     }

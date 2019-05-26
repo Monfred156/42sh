d59eef83246ffe611e33595a4246dafe4db35902
Bouhin Christophe <chbouhin@localhost.localdomain>
2019-05-26 22:56:34 +0200

    create function for take arg with getline

--- src/main_42sh.c
+++ src/main_42sh.c
@@ -17,28 +17,38 @@ char *clean_reduce_str(char *string)
     return (string);
 }
 
+char *take_arg(char *string)
+{
+    size_t len = 0;
+
+    free(string);
+    string = NULL;
+    if (isatty(0))
+        my_putstr("42sh> ");
+    if (getline(&string, &len, stdin) == -1)
+        return (NULL);
+    return (string);
+}
+
 int main_42sh(char **env)
 {
     char *string = NULL;
-    size_t len = 0;
     char **array;
     data_t data;
 
     get_env(env, &data);
     init_struct(&data);
     while (data.finish == false) {
-        if (isatty(0))
-            my_putstr("42sh> ");
-        if (getline(&string, &len, stdin) == -1)
+        string = take_arg(string);
+        if (string == NULL)
             break;
         data.history = add_one_line_end_of_array(data.history,
-            my_strdup(string));
+        my_strdup(string));
         string = clean_reduce_str(string);
         if (check_error(string) == 0) {
             array = parse_string(string);
             get_str(array, &data);
         }
-        string = NULL;
     }
     my_putstr("exit\n");
     return (data.to_return);

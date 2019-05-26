35a514abedd4097db80df9379eaa8ffea4718a60
Bouhin Christophe <chbouhin@localhost.localdomain>
2019-05-26 22:26:47 +0200

    bug fix if we enter one char

--- lib/my/detect_exec.c
+++ lib/my/detect_exec.c
@@ -9,7 +9,7 @@ int detect_exec(char *str)
 {
     if (str[0] == '.' && str[1] == '/')
         return (1);
-    for (int nb = 0; str[nb + 2] != '\0'; nb++)
+    for (int nb = 0; str[nb + 1] != '\0' && str[nb + 2] != '\0'; nb++) 
         if (str[nb] != '.' && str[nb + 1] == '.' && str[nb + 2] == '/')
             return (1);
     return (0);
